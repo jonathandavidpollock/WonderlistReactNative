daa23cf4273d70bd763e35aa35b8221ceb56a20b
Jonathan Pollock <jonathanpollock@Jonathans-MacBook-Pro.local>
2017-08-24 12:41:29 -0400

    adding styling to welcome page

--- source/containers/Secured.js
+++ source/containers/Secured.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ScrollView, Text, View, Button } from 'react-native';
+import { View, Text, Button,StyleSheet, Image } from 'react-native';
 import { logout } from '../redux/actions/auth';
+import avatar from '../img/avatar.png'
  
 class Secured extends Component {
     userLogout(e) {
@@ -11,16 +12,40 @@ class Secured extends Component {
      
     render() {
         return (
-            <ScrollView style={{padding: 20}}>
-                <Text style={{fontSize: 27}}>
-                    {`Welcome ${this.props.username}`}
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    {`Welcome, ${this.props.username}`}
                 </Text>
-                <View style={{margin: 20}}/>
+                <View style={styles.imgWrapper}>
+                    <Image source={avatar} style={styles.avatar}/>
+                </View>
                 <Button onPress={(e) => this.userLogout(e)} title="Logout"/>
-            </ScrollView>
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding:50,
+        flex: 1,
+        backgroundColor:'#222222',
+        flexDirection:'column'
+    },
+    welcome: {
+        fontSize:36,
+        textAlign:'center',
+        fontWeight:'700',
+        color:'#ffffff'
+    },
+    imgWrapper: {
+        justifyContent:'center',
+    },
+    avatar: {
+        width:100,
+        height:100
+    },
+})
  
  
 const mapStateToProps = (state, ownProps) => {
